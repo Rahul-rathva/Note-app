47cd898328a551121901788565388fc6f0bd9126
Rahul <160149113+Rahul-rathva@users.noreply.github.com>
2025-10-21 14:34:24 +0530

    connecting App with database

--- App/main.py
+++ App/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
+from app.db import create_db_and_tables
 
 app = FastAPI(title="Notes API")
 
 @app.get("/")
 def root():
-    return {"message": "My name is Rahul"}
     return {"message": "Welcome to the Notes API"}
+
+@app.on_event("startup")
+def on_statup():
+    create_db_and_tables()
