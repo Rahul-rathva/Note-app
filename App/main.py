7464fc34665b0537b91d2b2f0d3f8f29a8f9d3e9
Rahul <160149113+Rahul-rathva@users.noreply.github.com>
2025-10-22 12:10:47 +0530

    FastAPI Endpoints

--- App/main.py
+++ App/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.db import create_db_and_tables
+from app.crud import create_note, get_notes
 
 app = FastAPI(title="Notes API")
 
@@ -10,3 +11,10 @@ def root():
 @app.on_event("startup")
 def on_statup():
     create_db_and_tables()
+
+@app.post("/notes/", response_model=Note)
+def create_note_endpoint(note: Note):
+    return create_note(note)
+
+def list_notes():
+    return get_notes()
